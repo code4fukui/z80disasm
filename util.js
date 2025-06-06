fe4305cdd285e245f07e6a6e5c847e8cab97103a
Taisuke Fukuno <fukuno@jig.jp>
2025-06-07 06:19:35 +0900

    tab to space

--- util.js
+++ util.js
@@ -16,15 +16,15 @@ return "0x" + pad(value.toString(16), 4);
 }
 
 export function asByteHex(value){
-	if( Array.isArray(value)) {
-		let tmp = "";
-		for (let i = 0; i < value.length; i++) {
-			tmp += asByteHex(value[i]) + " ";
-		}
-		return tmp;
-	}
-	if (value == null) {
-		return "____";
-	}
+  if( Array.isArray(value)) {
+    let tmp = "";
+    for (let i = 0; i < value.length; i++) {
+      tmp += asByteHex(value[i]) + " ";
+    }
+    return tmp;
+  }
+  if (value == null) {
+    return "____";
+  }
   return "0x" + pad(value.toString(16), 2);
 }
