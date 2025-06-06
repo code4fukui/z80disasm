5c6344dd011a5699062ccb5023a67e0cc39ec5ec
Taisuke Fukuno <fukuno@jig.jp>
2025-06-07 05:58:37 +0900

    removed debug out

--- z80disasm.js
+++ z80disasm.js
@@ -2,7 +2,6 @@ import { Z80CPU } from "./z80cpu.js";
 
 export const z80disasm = (memory, pc) => {
   const z80 = Object.create(Z80CPU);
-  console.log(z80);
   z80.memory = memory;
   z80.registers.PC = pc;
   return z80.disassemble1();
