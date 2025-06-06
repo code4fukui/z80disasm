69e5d054eef07e0816e9f9fb8c84c3144c6cf025
Taisuke Fukuno <fukuno@jig.jp>
2025-06-07 06:10:39 +0900

    fixed style

--- util.js
+++ util.js
@@ -3,28 +3,28 @@ export  function pad(value, width) {
   return value.length >= width ? value : new Array(width - value.length + 1).join('0') + value;
 }
 
-export function asQwordHex(value){
-return "0x"+pad(value.toString(16),16);
+export function asQwordHex(value) {
+  return "0x" + pad(value.toString(16), 16);
 }
 
-export function asDwordHex(value){
-return "0x"+pad(value.toString(16),8);
+export function asDwordHex(value) {
+  return "0x" + pad(value.toString(16), 8);
 }
 
 export function asWordHex(value){
-return "0x"+pad(value.toString(16),4);
+return "0x" + pad(value.toString(16), 4);
 }
 
 export function asByteHex(value){
-	if(Array.isArray(value)){
-		var tmp="";
-		for(var i=0;i<value.length;i++){
-			tmp+=asByteHex(value[i])+" ";
+	if( Array.isArray(value)) {
+		let tmp = "";
+		for (let i = 0; i < value.length; i++) {
+			tmp += asByteHex(value[i]) + " ";
 		}
 		return tmp;
 	}
-	if(value==null){
+	if (value == null) {
 		return "____";
 	}
-return "0x"+pad(value.toString(16),2);
+  return "0x" + pad(value.toString(16), 2);
 }
